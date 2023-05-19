82913a69a4be4f9bf9c388f4dc6aeeaad6c57fda
Sedaylcn <syalcin1912@hotmail.com>
2023-05-19 01:15:52 +0100

    the third Commit

---
+++ src/test/java/Tests/Day10_TestNG_Framework/C08.java
@@ -0,0 +1,14 @@
+package Tests.Day10_TestNG_Framework;
+
+import org.testng.annotations.Test;
+
+public class C08 {
+
+      @Test
+    public void test01(){
+          System.out.println("deneme");
+
+      }
+
+
+}
